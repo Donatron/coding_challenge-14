46987459698aa22368a7ff677775550389f24d97
Donatron <don.macarthur68@gmail.com>
2019-02-12 10:18:48 +1000

    added images for all characters
    finished styling layout

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-document.querySelector("#game-button").addEventListener("click", () => {
-  alert("Click");
-});
+const footer = document.querySelector("#footer");
+
+// Add copyright tags to footer
+const generateCopyright = () => {
+  let html = "";
+  let date = new Date();
+  let year = date.getFullYear();
+
+  html += `<p>Copyright &copy ${year}`;
+  html += " | ";
+  html += "<a href='https://donatron.github.io/portfolio' target='_blank' >";
+  html += "Don Macarthur </a></p>";
+
+  return html;
+};
+
+footer.innerHTML = generateCopyright();
