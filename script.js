87e14747819fc791afd0acba58ea006329d6cd11
Donatron <don.macarthur68@gmail.com>
2019-02-15 10:03:33 +1000

    added functionality to generate random cards

--- script.js
+++ script.js
@@ -19,6 +19,105 @@ let cardTwoId;
 let match = false;
 let gameStart;
 
+// Create array of card image source files
+const imagesArray = [
+  "arya-stark.jpg",
+  "cersei-lannister.jpg",
+  "danaerys-targaeryan.jpg",
+  "jaime-lannister.jpg",
+  "jon-snow.jpg",
+  "ned-stark.jpg",
+  "sansa-stark.jpg",
+  "tyrion-lannister.jpg",
+  "arya-stark.jpg",
+  "cersei-lannister.jpg",
+  "danaerys-targaeryan.jpg",
+  "jaime-lannister.jpg",
+  "jon-snow.jpg",
+  "ned-stark.jpg",
+  "sansa-stark.jpg",
+  "tyrion-lannister.jpg"
+];
+
+// Get all available character names
+const getCharacterNames = () => {
+  const characters = [];
+  for (let image of imagesArray) {
+    characters.push(image);
+  }
+  return characters;
+};
+
+let characters = getCharacterNames();
+
+// Create array of random characters;
+const getRandomCharacters = () => {
+  let randomCharacters = [];
+
+  while (characters.length) {
+    let index = Math.floor(Math.random(characters.length) * characters.length);
+    randomCharacters.push(characters[index]);
+    characters.splice(index, 1);
+  }
+
+  return randomCharacters;
+};
+
+// Assign random characters to variable for creation of game board
+let randomCharacters = getRandomCharacters();
+
+// Declare function to create cards
+const createCard = (src, id) => {
+  let html = `<div class="card" id="card-${id}">`;
+  html += '<img src="img/iron-throne.png" alt="" class="tile side-a" />';
+  html += '<div class="side-b">';
+  html += `<img src="img/characters/${src}" alt="${src}" />`;
+  html += "</div></div>";
+
+  return html;
+};
+
+// Create game board
+const createBoard = () => {
+  let boardArray = [];
+  for (let i = 0; i < randomCharacters.length; i++) {
+    let card = createCard(randomCharacters[i], i);
+
+    boardArray.push(card);
+  }
+  return boardArray;
+};
+
+// Assign all cards to board;
+let board = createBoard();
+
+// Iterate over board array and add items to game board
+const displayBoard = () => {
+  // iterate over each row of game board
+  for (let i = 1; i <= 4; i++) {
+    let row = document.querySelector(`.row-${i} .tile-container`);
+
+    // Add four cards to each row
+    for (let j = 0; j < 4; j++) {
+      let card = board.shift();
+      row.innerHTML += card;
+    }
+  }
+};
+
+// Add start game button functionality
+gameButton.addEventListener("click", e => {
+  e.preventDefault();
+
+  // Display game board
+  displayBoard();
+
+  init();
+  // Start timer;
+  gameStart = new Date().getTime();
+  createTimer(gameStart);
+});
+
 // Reset all game variables;
 const resetGame = () => {
   numberOfMoves = 0;
@@ -61,13 +160,15 @@ const getCardVariables = (src, id) => {
 // Rotate Cards at click;
 cardsArray.forEach(card => {
   card.addEventListener("click", () => {
+    // Do nothing if card already matched
+    if (card.classList.contains("matched")) {
+      return;
+    }
     card.classList.toggle("flipped");
     flippedCards++;
 
     if (card.classList.contains("flipped")) {
       card.style.transform = "rotateY(180deg)";
-    } else {
-      card.style.transform = "rotateY(0deg)";
     }
 
     //assign photo src to variable
@@ -85,17 +186,27 @@ cardsArray.forEach(card => {
 
       if (checkMatch(cardOneSource, cardTwoSource)) {
         numberOfMatches++;
-        console.log(cardOne, cardTwo);
+
+        // add "matched" class to cards
+        cardOne.classList.add("matched");
+        cardTwo.classList.add("matched");
       } else {
-        setInterval(() => {
+        setTimeout(() => {
+          console.log("this is running");
           cardOne.style.transform = "rotateY(0deg)";
           cardTwo.style.transform = "rotateY(0deg)";
         }, 500);
+        // Remove "flipped" class
+        cardOne.classList.remove("flipped");
+        cardTwo.classList.remove("flipped");
       }
 
       // Update number of moves
       moves.innerHTML = numberOfMoves;
       matches.innerHTML = numberOfMatches;
+
+      // Reset flipped cards;
+      flippedCards = 0;
     }
   });
 });
@@ -114,16 +225,6 @@ const init = () => {
   stats.style.visibility = "visible";
 };
 
-// Add start game button functionality
-gameButton.addEventListener("click", e => {
-  e.preventDefault();
-
-  init();
-  // Start timer;
-  gameStart = new Date().getTime();
-  createTimer(gameStart);
-});
-
 // Add copyright tags to footer
 const generateCopyright = () => {
   let html = "";
@@ -139,3 +240,59 @@ const generateCopyright = () => {
 };
 
 footer.innerHTML = generateCopyright();
+
+// function to find card element
+const handleBoardClick = event => {
+  let card = event.target.parentNode;
+
+  // Do nothing if card already matched
+  if (card.classList.contains("matched")) {
+    return;
+  }
+  card.classList.toggle("flipped");
+  flippedCards++;
+
+  if (card.classList.contains("flipped")) {
+    card.style.transform = "rotateY(180deg)";
+  }
+
+  //assign photo src to variable
+  let sideB = card.querySelector(".side-b img");
+  let src = sideB.getAttribute("src");
+  let id = card.getAttribute("id");
+
+  // Get card Variables
+  getCardVariables(src, id);
+
+  if (flippedCards === 2) {
+    numberOfMoves++;
+    let cardOne = document.querySelector(`#${cardOneId}`);
+    let cardTwo = document.querySelector(`#${cardTwoId}`);
+
+    if (checkMatch(cardOneSource, cardTwoSource)) {
+      numberOfMatches++;
+
+      // add "matched" class to cards
+      cardOne.classList.add("matched");
+      cardTwo.classList.add("matched");
+    } else {
+      setTimeout(() => {
+        console.log("this is running");
+        cardOne.style.transform = "rotateY(0deg)";
+        cardTwo.style.transform = "rotateY(0deg)";
+      }, 500);
+      // Remove "flipped" class
+      cardOne.classList.remove("flipped");
+      cardTwo.classList.remove("flipped");
+    }
+
+    // Update number of moves
+    moves.innerHTML = numberOfMoves;
+    matches.innerHTML = numberOfMatches;
+
+    // Reset flipped cards;
+    flippedCards = 0;
+  }
+};
+
+document.querySelector(".board").addEventListener("click", handleBoardClick);
